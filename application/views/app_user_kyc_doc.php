08e2c249e4e56af101150d44c117b30a57b873a4
hemant-rioga <hemantkk@riogapremium.com>
2025-10-13 20:26:42 +0530

    code for added new changed

--- application/views/app_user_kyc_doc.php
+++ application/views/app_user_kyc_doc.php
@@ -75,6 +75,62 @@
 
            <?php }  ?>
 
+
+           <?php if($appuserdetails[0]['category_head']=='Owner'){ ?>
+
+
+              <!-- USER PHOTO -->
+                    <div class="col-4">
+                        <div class="card card-primary image">
+                            <div class="card-header"><h3 class="card-title">User Photo</h3></div>
+                            <div class="card-body p-10">
+                                <a href="<?php echo (isset($appuserdetails)) ? base_url().'uploads/user_photo/'.$appuserdetails[0]['user_photo']:'' ?>" data-toggle="lightbox" data-title="<?php echo (isset($appuserdetails)) ? 'USER PHOTO : '.$appuserdetails[0]['aadhar_card']:'' ?>">
+                                    <img src="<?php echo (isset($appuserdetails)) ? base_url().'uploads/user_photo/'.$appuserdetails[0]['user_photo']:'' ?>" class="img-fluid-thumbnail img-fluid mb-2" alt="USER PHOTO"/>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+
+                  <!-- ADDHAR CARD -->
+                    <div class="col-4">
+                        <div class="card card-primary image">
+                            <div class="card-header"><h3 class="card-title">Addhar Card</h3></div>
+                            <div class="card-body p-10">
+                                <a href="<?php echo (isset($appuserdetails)) ? base_url().'uploads/aadhar_card/'.$appuserdetails[0]['aadhar_card']:'' ?>" data-toggle="lightbox" data-title="<?php echo (isset($appuserdetails)) ? 'Adhar Card : '.$appuserdetails[0]['aadhar_card']:'' ?>">
+                                    <img src="<?php echo (isset($appuserdetails)) ? base_url().'uploads/aadhar_card/'.$appuserdetails[0]['aadhar_card']:'' ?>" class="img-fluid-thumbnail img-fluid mb-2" alt="ADDHAR CARD"/>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- PAN CARD -->
+                    <div class="col-4">
+                        <div class="card card-primary image">
+                            <div class="card-header"><h3 class="card-title">Pan Card</h3></div>
+                            <div class="card-body p-10">
+                                <a href="<?php echo (isset($appuserdetails)) ? base_url().'uploads/pan_card/'.$appuserdetails[0]['pan_card']:'' ?>" data-toggle="lightbox" data-title="<?php echo (isset($appuserdetails)) ? 'Pan Card : '.$appuserdetails[0]['aadhar_card']:'' ?>">
+                                    <img src="<?php echo (isset($appuserdetails)) ? base_url().'uploads/pan_card/'.$appuserdetails[0]['pan_card']:'' ?>" class="img-fluid-thumbnail img-fluid mb-2" alt="PAN CARD"/>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+
+            <!-- RC BOOK -->
+            <div class="col-4">
+                <div class="card card-primary image">
+                    <div class="card-header"><h3 class="card-title">RC BOOK</h3></div>
+                    <div class="card-body p-10">
+                        <a href="<?php echo (isset($appuserdetails)) ? base_url().'uploads/rc_book/'.$appuserdetails[0]['rc_book']:'' ?>" data-toggle="lightbox" data-title="<?php echo (isset($appuserdetails)) ? 'RC Book : '.$appuserdetails[0]['rc_book']:'' ?>">
+                            <img src="<?php echo (isset($appuserdetails)) ? base_url().'uploads/rc_book/'.$appuserdetails[0]['rc_book']:'' ?>" class="img-fluid-thumbnail img-fluid mb-2" alt="RC Book"/>
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+
+           <?php } ?>
           
 
             <!-- PAN CARD -->
